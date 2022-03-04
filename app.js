cc6056c4ed31b70aa139717e4c27c93ae5b9b60d
PaulaVirBurgos <paulavburgos@gmail.com>
2022-03-04 12:20:53 -0300

    terminando los cambios

--- app.js
+++ app.js
@@ -15,6 +15,38 @@ app.get('/babbage', (req, res) => {
     return res.sendFile(path.join(__dirname, 'views', 'babbage.html'))
 });
 
+app.get('/berners-lee', (req, res) => {
+
+    return res.sendFile(path.join(__dirname, 'views', 'berners-lee.html'))
+});
+
+app.get('/clarke', (req, res) => {
+
+    return res.sendFile(path.join(__dirname, 'views', 'clarke.html'))
+});
+
+app.get('/hamilton', (req, res) => {
+
+    return res.sendFile(path.join(__dirname, 'views', 'hamilton.html'))
+});
+
+app.get('/hopper', (req, res) => {
+
+    return res.sendFile(path.join(__dirname, 'views', 'hopper.html'))
+});
+
+app.get('/lovelace', (req, res) => {
+
+    return res.sendFile(path.join(__dirname, 'views', 'lovelace.html'))
+});
+
+app.get('/turing', (req, res) => {
+
+    return res.sendFile(path.join(__dirname, 'views', 'turing.html'))
+});
+
+
+
 app.listen(3030, () => {
 
     console.log(`servidor corriendo en el puerto http://localhost:${port}`);
